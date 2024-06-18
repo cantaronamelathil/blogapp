36bdbe3f108aa7329c36472c03087fcaa5915712
cantaronamelathil <cantaronamelathil@gmail.com>
2024-06-18 12:17:33 +0530

    blog commit

---
+++ src/components/home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Review from './review/review'
+function Home() {
+  return (
+ 
+        <main>
+            <section className='container'>
+                <div className='title'>
+                    <h2>
+                        blog app
+                    </h2>
+
+                     <div className='underline'>
+
+                     </div>
+                     <Review/>
+                </div>
+
+                 
+
+
+
+
+            </section>
+        </main>
+
+
+   
+  )
+}
+
+export default Home
